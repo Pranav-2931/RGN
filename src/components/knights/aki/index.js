6b52f0cc9740bc8468f4da640b53a0a2183325b8
Pranav Raj <pranavrajkichu@gmail.com>
2026-04-30 16:24:46 +0530

    fix: host Aki background gif locally for 100% reliability

--- src/components/knights/aki/index.js
+++ src/components/knights/aki/index.js
@@ -1,7 +1,9 @@
+import akiBg from '../../assets/aki-bg.gif'
+
 const akiData = {
   name: 'AKI',
   role: 'KNIGHT',
-  bgUrl: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Z4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/Mv7YkL5oO9F5M0W7WJ/giphy.gif', // Reliable Giphy direct link
+  bgUrl: akiBg, // Using local file for 100% reliability
   theme: { primary: '#4ade80', secondary: '#166534' }, // Green theme for Aki
   stats: [
     { label: 'Total Kills', value: '20,000+' },
