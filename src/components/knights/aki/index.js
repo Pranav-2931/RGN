4527514b8bf23a16c21eb961b4eeab02ea04ae8d
Pranav Raj <pranavrajkichu@gmail.com>
2026-04-30 16:22:58 +0530

    fix: switched Aki background to reliable Giphy link

--- src/components/knights/aki/index.js
+++ src/components/knights/aki/index.js
@@ -1,7 +1,7 @@
 const akiData = {
   name: 'AKI',
   role: 'KNIGHT',
-  bgUrl: 'https://media.tenor.com/Xf-E0Jv8p6AAAAAM/isagi-yoichi-isagi-nel.gif', // High quality direct link
+  bgUrl: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Z4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/Mv7YkL5oO9F5M0W7WJ/giphy.gif', // Reliable Giphy direct link
   theme: { primary: '#4ade80', secondary: '#166534' }, // Green theme for Aki
   stats: [
     { label: 'Total Kills', value: '20,000+' },
