24dc8c10376533e8f1593530b8a172624d75277d
Pranav Raj <pranavrajkichu@gmail.com>
2026-04-30 16:11:42 +0530

    fix: updated Aki background link and increased visibility

--- src/components/knights/aki/index.js
+++ src/components/knights/aki/index.js
@@ -1,7 +1,7 @@
 const akiData = {
   name: 'AKI',
   role: 'KNIGHT',
-  bgUrl: 'https://media.tenor.com/Xf-E0Jv8p6AAAAAC/isagi-yoichi-isagi-nel.gif', // Updated direct link
+  bgUrl: 'https://media.tenor.com/Xf-E0Jv8p6AAAAAM/isagi-yoichi-isagi-nel.gif', // High quality direct link
   theme: { primary: '#4ade80', secondary: '#166534' }, // Green theme for Aki
   stats: [
     { label: 'Total Kills', value: '20,000+' },
