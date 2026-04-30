b48c237a953f589dd7e5dc087ca13ae795597953
Pranav Raj <pranavrajkichu@gmail.com>
2026-04-30 16:33:23 +0530

    fix: removed large local gif and updated Aki background with new link

--- src/components/knights/aki/index.js
+++ src/components/knights/aki/index.js
@@ -1,9 +1,7 @@
-import akiBg from '../../assets/aki-bg.gif'
-
 const akiData = {
   name: 'AKI',
   role: 'KNIGHT',
-  bgUrl: akiBg, // Using local file for 100% reliability
+  bgUrl: 'https://media.tenor.com/tZ2eYtD3pWwAAAAC/isagi-yoichi-blue-lock.gif', // New direct link for Isagi GIF
   theme: { primary: '#4ade80', secondary: '#166534' }, // Green theme for Aki
   stats: [
     { label: 'Total Kills', value: '20,000+' },
